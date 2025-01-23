82fbf9a51abab8d5eaa27bfd90614a9e65b39f1e
Aman Dabral <amandabral5@gmail.com>
2025-01-23 21:28:39 +0530

    Updated App Files

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h2>Testing Auto Deployment of my code using Github actions.</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
